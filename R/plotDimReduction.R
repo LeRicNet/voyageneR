db01ef08c63df081acefe5482a2fb60ef283bcf8
lericnet <ericprince82@gmail.com>
2024-02-28 10:49:31 -0700

    update plotDimReduction

--- R/plotDimReduction.R
+++ R/plotDimReduction.R
@@ -1,8 +1,8 @@
 plotDimReduction <- function() {
   acp_sn_umap <- data('acp_sn_umap')
   dim_reduction <- data.frame(
-    'UMAP1' = unlist(acp_sn_umap[,1]),
-    'UMAP2' = unlist(acp_sn_umap[,2])
+    'UMAP1' = as.numeric(acp_sn_umap[,1]),
+    'UMAP2' = as.numeric(acp_sn_umap[,2])
   )
   return(
     dim_reduction
