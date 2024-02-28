e6d32574680d0059cdab118cf75f6e0cca7bb908
lericnet <ericprince82@gmail.com>
2024-02-28 11:18:38 -0700

    update plotDimReduction

--- R/plotDimReduction.R
+++ R/plotDimReduction.R
@@ -1,7 +1,8 @@
 plotDimReduction <- function() {
   dim_reduction <- data.frame(
     'UMAP1' = as.data.frame(acp_sn_umap)$UMAP_1,
-    'UMAP2' = as.data.frame(acp_sn_umap)$UMAP_2
+    'UMAP2' = as.data.frame(acp_sn_umap)$UMAP_2,
+    'color' = as.data.frame(acp_sn_meta)$celltypes
   )
   return(
     dim_reduction
