5bdf4d5c0bc924c222e74c45c25e924a92fdcd97
lericnet <ericprince82@gmail.com>
2024-02-28 11:33:45 -0700

    add meta

--- R/plotDimReduction.R
+++ R/plotDimReduction.R
@@ -1,8 +1,16 @@
 plotDimReduction <- function() {
+
+  # Make Color Ramp
+  original_colors <- c("red", "blue")
+  color_palet <- colorRampPalette(original_colors)
+  color_index <- as.integer(as.factor(acp_sn_meta$celltypes))
+  colors <- color_palet(max(color_index))
+
   dim_reduction <- data.frame(
     'UMAP1' = as.data.frame(acp_sn_umap)$UMAP_1,
     'UMAP2' = as.data.frame(acp_sn_umap)$UMAP_2,
-    'color' = as.data.frame(acp_sn_meta)$celltypes
+    'category' = acp_sn_meta$celltypes,
+    'color' = colors[color_index]
   )
   return(
     dim_reduction
