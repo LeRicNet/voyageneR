fa1a85fa8ee0dea939ffa4fa2e10beceb1472322
lericnet <ericprince82@gmail.com>
2024-02-28 14:30:34 -0700

    update plotDimReduction

--- R/plotDimReduction.R
+++ R/plotDimReduction.R
@@ -1,18 +1,22 @@
-plotDimReduction <- function() {
-
-  # Make Color Ramp
-  original_colors <- c("red", "blue")
-  color_palet <- colorRampPalette(original_colors)
-  color_index <- as.integer(as.factor(acp_sn_meta$celltypes))
-  colors <- color_palet(max(color_index))
-
+plotDimReduction <- function(
+    reduction = 'umap',
+    color_by = 'celltypes'
+    ) {
   dim_reduction <- data.frame(
-    'UMAP1' = as.data.frame(acp_sn_umap)$UMAP_1,
-    'UMAP2' = as.data.frame(acp_sn_umap)$UMAP_2,
-    'category' = acp_sn_meta$celltypes,
-    'color' = colors[color_index]
+    'Dim1' = as.data.frame(acp_sn_umap)$UMAP_1,
+    'Dim2' = as.data.frame(acp_sn_umap)$UMAP_2,
+    'category' = acp_sn_meta[,color_by],
+    'color' = .buildColorRamp(color_by)
   )
   return(
     dim_reduction
   )
+}
+
+.buildColorRamp <- function(color_by) {
+  original_colors <- c("red", "blue")
+  color_palet <- colorRampPalette(original_colors)
+  color_index <- as.integer(as.factor(acp_sn_meta[,color_by]))
+  colors <- color_palet(max(color_index))
+  return(colors[color_index])
 }
