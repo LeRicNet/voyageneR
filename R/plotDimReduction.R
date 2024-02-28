6e034199cda176abae19b8f36761d05ad087ab10
lericnet <ericprince82@gmail.com>
2024-02-28 14:41:09 -0700

    update plotDimReduc

--- R/plotDimReduction.R
+++ R/plotDimReduction.R
@@ -1,7 +1,26 @@
+#' Plot Dimension Reduction
+#'
+#' This function creates a data frame for plotting dimension reduction methods
+#' such as UMAP or t-SNE.
+#'
+#' @param reduction A character string specifying the type of dimension reduction
+#'  to plot. Default is 'umap'.
+#' @param color_by A character string specifying the variable in the metadata
+#' to color cells by. Default is 'celltypes'.
+#'
+#' @return A data frame with columns 'Dim1', 'Dim2', 'category', and 'color'.
+#'  'Dim1' and 'Dim2' correspond to the two dimensions of the UMAP plot.
+#'   'category' corresponds to the cell type or other category specified
+#'   by 'color_by'. 'color' is determined by the .buildColorRamp function.
+#'
+#' @examples
+#' plotDimReduction(reduction = 'umap', color_by = 'celltypes')
+#'
+#' @export
 plotDimReduction <- function(
     reduction = 'umap',
     color_by = 'celltypes'
-    ) {
+) {
   dim_reduction <- data.frame(
     'Dim1' = as.data.frame(acp_sn_umap)$UMAP_1,
     'Dim2' = as.data.frame(acp_sn_umap)$UMAP_2,
