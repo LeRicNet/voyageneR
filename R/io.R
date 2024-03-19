f815a037fd0132b6eccc9b35ce4ef8b7be4bb7b4
lericnet <ericprince82@gmail.com>
2024-03-19 14:47:50 -0600

    update docs

--- R/io.R
+++ R/io.R
@@ -1,3 +1,13 @@
+#' Load a Seurat object
+#'
+#' This function loads a Seurat object from a specified file.
+#'
+#' @return A Seurat object loaded from "/home/opencpu/data/acp_scn_annotated.rds".
+#' @export
+#'
+#' @examples
+#' loadSeuratObject()
+#'
 loadSeuratObject <- function() {
   seurat_object <- readRDS("/home/opencpu/data/acp_scn_annotated.rds")
   return(seurat_object)
