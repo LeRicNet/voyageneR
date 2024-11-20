e7683512f682bcc10e9a3fc99f70f9d3c283753c
Eric Prince <ericprince82@gmail.com>
2024-11-20 14:27:19 -0700

    add getIdentities

--- R/getIdentities.R
+++ R/getIdentities.R
@@ -1,5 +1,10 @@
 getIdentities <- function() {
+
+  cnames <- colnames(seurat_object@meta.data)
+  cnames <- cnames[!grepl('nCount', cnames)]
+  cnames <- cnames[!grepl('nFeature', cnames)]
+
   return(data.frame(
-    'identities' = colnames(seurat_object@meta.data)
+    'identities' = cnames
   ))
 }
