e7c84b17b581ff8fe43453b09c12f2acf47bc6bb
Eric Prince <ericprince82@gmail.com>
2024-11-20 15:56:14 -0700

    add seurat_wrapper

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
-.onLoad <- function(libname, pkgname) {
-  # data_path <- system.file("data", "yourdata.rds", package = pkgname)
-  seurat_object <- loadSeuratObject()
-  expression_matrix <- seurat_object@assays$integrated@scale.data
-  assign("seurat_object", seurat_object, envir = .GlobalEnv)
-  assign("expression_matrix", expression_matrix, envir = .GlobalEnv)
-}
+# .onLoad <- function(libname, pkgname) {
+#   # data_path <- system.file("data", "yourdata.rds", package = pkgname)
+#   seurat_object <- loadSeuratObject()
+#   expression_matrix <- seurat_object@assays$integrated@scale.data
+#   assign("seurat_object", seurat_object, envir = .GlobalEnv)
+#   assign("expression_matrix", expression_matrix, envir = .GlobalEnv)
+# }
